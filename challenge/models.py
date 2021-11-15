de2c23281aa940b155af8f503576c15d7343099a
Mike Douglas <mikedoouglas@hotmail.com>
2021-11-15 19:43:25 -0300

    Create models

--- challenge/models.py
+++ challenge/models.py
@@ -1,2 +1,21 @@
+import uuid
+
 from django.db import models
 
+
+class Author(models.Model):
+    id = models.UUIDField("id", primary_key=True, default=uuid.uuid4,
+                          editable=False)
+    name = models.TextField("name")
+    picture = models.URLField("picture")
+
+
+class Article(models.Model):
+    id = models.UUIDField("id", primary_key=True, default=uuid.uuid4,
+                          editable=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.TextField("category")
+    title = models.TextField("title")
+    summary = models.TextField("summary")
+    firstParagraph = models.TextField("firstParagraph")
+    body = models.TextField("body")
