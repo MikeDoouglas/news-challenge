776e35e97ed06c7294d38b7f1f4f1382d40306f5
Mike Douglas <mikedoouglas@hotmail.com>
2021-11-16 23:39:51 -0300

    Adjustments to use environment variables file

--- djangochallenge/settings.py
+++ djangochallenge/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,10 +87,10 @@ WSGI_APPLICATION = 'djangochallenge.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'challenge',
-        'USER': 'postgres',
-        'PASSWORD': 'challenge',
-        'HOST': '127.0.0.1',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
